b2013feef356678749cbb00bd27b5e72adb85698
marcel@macBook <marcel.benders@icloud.com>
2018-11-09 07:58:05 +0100

    assets iOS

---
+++ shake-A-lunch.std/AppStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shake_A_lunch.std
+{
+    public class AppStore
+    {
+        protected AppStore()
+        {
+            Locations = new List<string>();
+            Rnd = new Random(DateTime.Now.Millisecond);
+        }
+
+        static readonly Lazy<AppStore> instance = new Lazy<AppStore>(() => new AppStore());
+        public static AppStore Instance => instance.Value;
+        public IList<string> Locations { get; set; }
+        public Random Rnd { get; set; }
+    }
+}
